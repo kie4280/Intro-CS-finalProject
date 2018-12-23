7ae2b2cf2de8e1cc9abf9a6b77585a0a6079a45e
kie4280 <kie4280ann@gmail.com>
2018-12-23 16:44:08 +0800

    able to store token and list the things in the root folder

--- google.py
+++ google.py
@@ -1,36 +1,86 @@
 import os
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
-from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.http import MediaFileUpload
+from httplib2 import Http
+from oauth2client import file, client, tools
+import pprint
 
 
 class GoogleDriveApi:
-# The CLIENT_SECRETS_FILE variable specifies the name of a file that contains
-# the OAuth 2.0 information for this application, including its client_id and
-# client_secret.
-CLIENT_SECRETS_FILE = "client_secret.json"
+    # The CLIENT_SECRETS_FILE variable specifies the name of a file that contains
+    # the OAuth 2.0 information for this application, including its client_id and
+    # client_secret.
+    CLIENT_SECRETS_FILE = "client_secret.json"
 
-# This access scope grants read-only access to the authenticated user's Drive
-# account.
-SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
-API_SERVICE_NAME = 'drive'
-API_VERSION = 'v3'
+    # This access scope grants read-only access to the authenticated user's Drive
+    # account.
+    SCOPES = ['https://www.googleapis.com/auth/drive']
+    API_SERVICE_NAME = 'drive'
+    API_VERSION = 'v3'
+    API_SERVICE = None
+    current_dir="/"
+    folderMap=dict()
 
-   def get_authenticated_service():
-        flow = InstalledAppFlow.from_client_secrets_file(
-            CLIENT_SECRETS_FILE, SCOPES)
-        credentials = flow.run_local_server(host='localhost',         port=8080,
-          authorization_prompt_message='Please visit this URL: {url}',
-                                            success_message='The auth flow is complete; you may close this window.',
-                                            open_browser=True)
-        return build(API_SERVICE_NAME, API_VERSION, credentials=credentials)
+    def __init__(self):
+        if self.API_SERVICE == None:
+            self.API_SERVICE = self.get_authenticated_service()
 
-    def list_drive_files(service, **kwargs):
-        results = service.files().list(**kwargs).execute()
+    def get_authenticated_service(self):
+        store = file.Storage('token.json')
+        creds = store.get()
+        if not creds or creds.invalid:
+            flow = client.flow_from_clientsecrets('client_secret.json', self.SCOPES)
+            creds = tools.run_flow(flow, store)
+        service = build(self.API_SERVICE_NAME, self.API_VERSION, http=creds.authorize(Http()))
+        return service
+
+    def list_content(self, id, trashed="false", **kwargs):
+        results = self.API_SERVICE.files().list(q=u"'{0}' in parents and trashed={1}".format(id, trashed),\
+        orderBy="folder, name", pageSize=50, spaces="drive", fields="nextPageToken, files(id, name)", **kwargs)\
+        .execute()
+        items = results.get("files", [])
+        if len(items) > 0:
+            for item in items:
+                # self.addMap()
+                pass
+
         return results
+    
+    def folder_list(self, path):
+        pass
+    
+    def addMap(self, name, id):
+        self.folderMap.get()
+        pass
+    
 
-    def createFolder(self):
+
+    def parsePath(self, path):
+        path=path.lstrip().rstrip()
+        folds=path.split('/')
+        folds=folds[1:]
+        if len(folds) == 1:
+            return "root"
+        elif len(folds) > 1:
+            print(folds)
+
+
+
+    def createFolder(self, des, name):
+        file_metadata = {"name": name,
+                         "mimeType": "application/vnd.google-apps.folder"}
+        exec = self.API_SERVICE.files().create(body=file_metadata, fields="id")\
+            .execute()
+        print(exec.get("id"))
+
+    def createFile(self, des, file):
+        file1 = MediaFileUpload("client_secret.json", mimetype="text/json")
         pass
 
-    def createFile(self, file):
-        pass
+
+g = GoogleDriveApi()
+pprint.PrettyPrinter(indent=2).pprint(g.list_content("root"))
+g.parsePath("/roo/g")
+# g.createFolder("fuck")
+# g.createFile(open("client_secret.json"))

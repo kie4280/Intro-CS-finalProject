f3cdfefcbac9b9d914499734b82853513527cc8e
kie4280 <kie4280ann@gmail.com>
2019-01-10 10:57:10 +0800

    update branch

--- app/final.py
+++ app/final.py
@@ -4,10 +4,11 @@ import re
 g = GoogleDriveApi()
 while True:
     user_in = input(g.pwd()+">")    
-    args=re.findall(r"[\^\s]*[\"\']*([\w.-/\\]+)[\"\']*[\s\$]*", user_in)
+    # args=re.findall(r"[\^\s]*[\"\']*([\w.-/\\]+)[\"\']*[\s\$]*", user_in)
+    args=re.findall(r"^|[\s\"\']*([\w.-/\\]+)[\"\']*(?=\s+|$)", user_in)
     command=args[0]
     # args=args[1:]
-    # print(args)
+    print(args)
     cd_dir = re.match(r"\s*cd\s*[\"']*([\w./\s]+)[\"']*\s*$", user_in)
     ls_dir = re.match(r"\s*ls\s*$", user_in)
     if cd_dir != None:

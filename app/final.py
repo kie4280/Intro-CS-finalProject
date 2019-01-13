374ffb8887b57e94438088b9b5d4b9d423f20b71
kie4280 <kie4280ann@gmail.com>
2019-01-13 13:10:53 +0800

    command interpretor regex fully functional

--- app/final.py
+++ app/final.py
@@ -1,29 +1,45 @@
 from googleDrive import GoogleDriveApi, GoogleDriveApiException
 import pprint
 import re
+
 g = GoogleDriveApi()
+
+def empty(x):
+    if x==None:
+        return False
+    else:
+        return True
 while True:
     user_in = input(g.pwd()+">")    
     args=re.findall(r"(?:^|\s)*(?:[\"\']+?([\s\w.-/\\]+)[\"\']+?|([\w.-/\\]+))(?:^|\s)*", user_in)
-    command=args[0]
-    # args=args[1:]
-    print(args)
-    cd_dir = re.search(r"cd\s*[\"']*([\w./\s]+)[\"']*\s*$", user_in)
-    ls_dir = re.search(r"ls\s*$", user_in)
+    if len(args)==0:
+        continue
+    command=args[0][1]    
+    args_q=list(filter(empty, [q[0] if q[0]!='' else None for q in args[1:]]))
+    args_nq=list(filter(empty, [q[1] if q[1]!='' else None for q in args[1:]]))
+    print(args_q, args_nq)
+    
     # download = re.search(r"download\s*([\w.-/\\]+)")
-    if cd_dir != None:
-        args = cd_dir.group(1)
+    if command == "cd":
+        
         try:
-            g.to_folder(args)
+            if len(args_nq)>0:
+                g.to_folder(args_nq[0])
+            elif len(args_q)>0:
+                g.to_folder(args_q[0])
+            
         except GoogleDriveApiException as e:            
             if e.args[0]==GoogleDriveApiException.NO_DIR:
                 print("No such directory!!")
-    elif ls_dir != None:
+    elif command == "ls":
         try:
             pprint.PrettyPrinter(indent=2).pprint(g.list_content())
         except GoogleDriveApiException as e:
             if e.args[0]==GoogleDriveApiException.NOT_SIGNIN_EXP:
                 print("You are not signed in. To sign in type the command 'signin'")
+
+    elif command =="download":
+        pass
     if command == "exit":
         exit()
     elif command == "signin":
